1e4b2929bc69e30a413be03bb8e9a7e6cf52bc3b
lzj1769 <lzj1769@gmail.com>
2023-10-16 23:26:52 -0400

    add

---
+++ src/deep_tf_v1/.ipynb_checkpoints/train-checkpoint.py
@@ -0,0 +1,229 @@
+import argparse
+import os
+import sys
+import numpy as np
+import pandas as pd
+import warnings
+import torch
+import logging
+from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
+
+from model import DeepTensorFactorization
+from dataset import get_dataloader
+from utils import set_seed, get_cell_type_compound_gene, compute_mrrmse
+import config
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+    
+    
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Required parameters
+    parser.add_argument("--valid_cell_type", type=str, default='nk',
+                        help="Which cell type for validation. Available options are: nk,  t_cd4, t_cd8, t_reg")
+    parser.add_argument("--log",
+                        action="store_true",
+                        help='write training history')
+    parser.add_argument("--resume",
+                        action="store_true",
+                        help='training model from check point')
+    parser.add_argument("--batch_size", default=50000, type=int,
+                        help="Batch size.")
+    parser.add_argument("--epochs", default=100, type=int,
+                        help="Total number of training epochs to perform.")
+    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+
+    return parser.parse_args()
+
+
+def train(model, dataloader, criterion, optimizer, device):
+    model.train()
+    
+    train_loss = 0.0
+    cell_types, compunds, genes, targets, preds = list(), list(), list(), list(), list()
+    for (cell_type_indices, compound_indices, gene_indices, target) in dataloader:
+        pred = model(cell_type_indices.to(device), 
+                     compound_indices.to(device), 
+                     gene_indices.to(device))
+        loss = criterion(pred.view(-1), target.to(device))
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        train_loss += loss.item() / len(dataloader)
+        
+        # collect data to compute mrrmse
+        cell_types.append(cell_type_indices.view(-1).tolist())
+        compunds.append(compound_indices.view(-1).tolist())
+        genes.append(gene_indices.view(-1).tolist())
+        targets.append(target.view(-1).tolist())
+        preds.append(pred.detach().cpu().view(-1).tolist())
+        
+    df = pd.DataFrame(data={'cell_type': np.concatenate(cell_types),
+                            'sm_name': np.concatenate(compunds),
+                            'gene': np.concatenate(genes),
+                            'target': np.concatenate(targets),
+                            'predict': np.concatenate(preds)})
+    
+    mrrmse = compute_mrrmse(df)
+    
+    return train_loss, mrrmse
+        
+     
+def valid(model, dataloader, criterion, device):
+    model.eval()
+    
+    valid_loss = 0.0
+    cell_types, compunds, genes, targets, preds = list(), list(), list(), list(), list()
+    for (cell_type_indices, compound_indices, gene_indices, target) in dataloader:
+        pred = model(cell_type_indices.to(device), 
+                     compound_indices.to(device), 
+                     gene_indices.to(device))
+        
+        loss = criterion(pred.view(-1), target.to(device))
+        
+        valid_loss += loss.item() / len(dataloader)
+        
+        # collect data to compute mrrmse
+        cell_types.append(cell_type_indices.view(-1).tolist())
+        compunds.append(compound_indices.view(-1).tolist())
+        genes.append(gene_indices.view(-1).tolist())
+        targets.append(target.view(-1).tolist())
+        preds.append(pred.detach().cpu().view(-1).tolist())
+        
+
+    df = pd.DataFrame(data={'cell_type': np.concatenate(cell_types),
+                            'sm_name': np.concatenate(compunds),
+                            'gene': np.concatenate(genes),
+                            'target': np.concatenate(targets),
+                            'predict': np.concatenate(preds)})
+    
+    mrrmse = compute_mrrmse(df)
+    
+    return valid_loss, mrrmse      
+
+   
+def main():
+    args = parse_args()
+
+    # set random seed
+    set_seed(args.seed)
+
+    # Setup CUDA, GPU
+    device = torch.device("cuda")
+    logging.info(f'Validation cell type: {args.valid_cell_type}')
+    
+    # Setup model
+    cell_types, compounds, genes = get_cell_type_compound_gene()
+    
+    model = DeepTensorFactorization(cell_types=cell_types,
+                                    compounds=compounds,
+                                    genes=genes)
+    model_path = os.path.join(config.MODEL_PATH,
+                              f'valid_cell_type_{args.valid_cell_type}.pth')
+    
+    if args.resume:
+        state_dict = torch.load(model_path)
+        model.load_state_dict(state_dict['state_dict'])
+        
+        train_loss = state_dict['train_loss']
+        valid_loss = state_dict['valid_loss']
+        train_mrrmse = state_dict['train_mrrmse']
+        valid_mrrmse = state_dict['valid_mrrmse']
+        
+        logging.info(f'Loading model from check point!')
+        logging.info(f'Train loss: {train_loss: .03f}; valid loss: {valid_loss: .03f}; train mrrmse: {train_mrrmse: .03f}; valid mrrmse: {valid_mrrmse: .03f}')
+        
+    model.to(device)
+    
+    # Setup data
+    logging.info(f'Loading data')
+    
+    df_train = pd.read_csv(f"{config.RESULTS_DIR}/df_train_{args.valid_cell_type}.csv") 
+    df_valid = pd.read_csv(f"{config.RESULTS_DIR}/df_valid_{args.valid_cell_type}.csv") 
+    df_train.target = df_train.target.astype(np.float32)
+    df_valid.target = df_valid.target.astype(np.float32)
+
+    train_loader = get_dataloader(df=df_train, 
+                                  cell_types=cell_types, 
+                                  compounds=compounds,
+                                  genes=genes,
+                                  batch_size=args.batch_size,
+                                  num_workers=2,
+                                  drop_last=False,
+                                  shuffle=True,
+                                  train=True)
+    
+    valid_loader = get_dataloader(df=df_valid, 
+                                  cell_types=cell_types, 
+                                  compounds=compounds,
+                                  genes=genes,
+                                  batch_size=args.batch_size,
+                                  num_workers=2,
+                                  drop_last=False,
+                                  shuffle=False,
+                                  train=True)
+
+    # Setup loss and optimizer
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=1e-3,
+                                 weight_decay=1e-4)
+    
+    """ Train the model """
+    dt_string = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+    log_prefix = f'valid_cell_type_{args.valid_cell_type}_{dt_string}'
+    log_dir = os.path.join(config.TRAINING_LOG_PATH,
+                           log_prefix)
+    
+
+    tb_writer = SummaryWriter(log_dir=log_dir)
+        
+    logging.info(f'Training started')
+    
+    if args.resume:
+        best_valid_mrrmse = valid_mrrmse
+    else:
+        best_valid_mrrmse = 100
+        
+    for epoch in range(args.epochs):
+        train_loss, train_mrrmse = train(
+            dataloader=train_loader,
+            model=model,
+            criterion=criterion,
+            optimizer=optimizer,
+            device=device)
+        
+        valid_loss, valid_mrrmse = valid(
+            dataloader=valid_loader,
+            model=model,
+            criterion=criterion,
+            device=device)
+        
+        tb_writer.add_scalar("Training loss", train_loss, epoch)
+        tb_writer.add_scalar("Valid loss", valid_loss, epoch)
+        tb_writer.add_scalar("Trianing MRRMSE", train_mrrmse, epoch)
+        tb_writer.add_scalar("Vliad MRRMSE", valid_mrrmse, epoch)
+            
+        if valid_mrrmse < best_valid_mrrmse:
+            best_valid_mrrmse = valid_mrrmse
+            state = {'state_dict': model.state_dict(),
+                     'train_loss': train_loss,
+                     'valid_loss': valid_loss,
+                     'train_mrrmse': train_mrrmse,
+                     'valid_mrrmse': valid_mrrmse}
+            torch.save(state, model_path)
+        
+    logging.info(f'Training finished')
+ 
+if __name__ == "__main__":
+    main()
---
+++ src/deep_tf_v2/train.py
@@ -0,0 +1,134 @@
+import argparse
+import os
+import sys
+import numpy as np
+import pandas as pd
+import warnings
+import torch
+import logging
+from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
+
+from model import DeepTensorFactorization
+from dataset import get_dataloader
+from utils import set_seed, get_cell_type_compound_gene, compute_mrrmse
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Required parameters
+    parser.add_argument("--batch_size", default=1000, type=int,
+                        help="Batch size. Default 1000")
+    parser.add_argument("--epochs", default=100, type=int,
+                        help="Total number of training epochs to perform. Default: 100")
+    parser.add_argument("--lr", default=1e-03, type=float,
+                        help="Learning rate. Default: 0.001")
+    parser.add_argument("--seed", type=int, default=42,
+                        help="random seed for initialization")
+
+    return parser.parse_args()
+
+
+def train(model, dataloader, reg_criterion, cls_criterion, optimizer, device):
+    model.train()
+
+    train_reg_loss, train_cls_loss = 0.0, 0.0
+
+    for (cell_type_indices, compound_indices, gene_indices, target_p_value, target_sign) in dataloader:
+        p_value, sign = model(cell_type_indices.to(device),
+                              compound_indices.to(device),
+                              gene_indices.to(device))
+        
+        reg_loss = reg_criterion(p_value.view(-1), target_p_value.to(device))
+        cls_loss = cls_criterion(sign.view(-1), target_sign.to(device))
+
+        loss = reg_loss + cls_loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        train_reg_loss += reg_loss.item() / len(dataloader)
+        train_cls_loss += cls_loss.item() / len(dataloader)
+
+    return train_reg_loss, train_cls_loss
+
+
+def main():
+    args = parse_args()
+
+    # set random seed
+    set_seed(args.seed)
+
+    # Setup CUDA, GPU
+    device = torch.device("cuda")
+
+    # Setup model
+    cell_types, compounds, genes = get_cell_type_compound_gene()
+
+    model = DeepTensorFactorization(cell_types=cell_types,
+                                    compounds=compounds,
+                                    genes=genes)
+    model_path = '../../model/deep_embedding/model.pth'
+    model.to(device)
+
+    # Setup data
+    logging.info(f'Loading data')
+    df_train = pd.read_csv(f"../../results/deep_tf/train.csv")
+    df_train.target = df_train.target.astype(np.float32)
+
+    train_loader = get_dataloader(df=df_train,
+                                  cell_types=cell_types,
+                                  compounds=compounds,
+                                  genes=genes,
+                                  batch_size=args.batch_size,
+                                  num_workers=2,
+                                  drop_last=False,
+                                  shuffle=True,
+                                  train=True)
+
+    # Setup loss and optimizer
+    reg_criterion = torch.nn.MSELoss()
+    cls_criterion = torch.nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam([param for param in model.parameters() if param.requires_grad == True],
+                                 lr=1e-3,
+                                 weight_decay=1e-4)
+
+    """ Train the model """
+    dt_string = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+    log_prefix = f'{dt_string}'
+    log_dir = os.path.join('./log', log_prefix)
+    tb_writer = SummaryWriter(log_dir=log_dir)
+
+    logging.info(f'Training started')
+    for epoch in range(args.epochs):
+        reg_loss, cls_loss = train(
+            dataloader=train_loader,
+            model=model,
+            reg_criterion=reg_criterion,
+            cls_criterion=cls_criterion,
+            optimizer=optimizer,
+            device=device)
+
+        tb_writer.add_scalar("Regression loss", reg_loss, epoch)
+        tb_writer.add_scalar("Classification mrrmse", cls_loss, epoch)
+
+        state = {'state_dict': model.state_dict(),
+                 'reg_loss': reg_loss,
+                 'cls_loss': cls_loss}
+        torch.save(state, model_path)
+
+    logging.info(f'Training finished')
+
+
+if __name__ == "__main__":
+    main()
